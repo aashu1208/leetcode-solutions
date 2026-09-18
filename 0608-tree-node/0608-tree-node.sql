2f94670117054985dbee05cc2a967ff559bf0da9
Ashutosh Sharma <86849270+aashu1208@users.noreply.github.com>
2026-09-18 18:41:46 +0530

    Time: 588 ms (29.02%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -2,7 +2,9 @@
 SELECT id,
        CASE
            WHEN p_id IS null THEN 'Root'
-           WHEN id NOT IN (SELECT DISTINCT p_id FROM tree WHERE p_id IS NOT NULL) THEN 'Leaf'
+           WHEN id NOT IN (SELECT DISTINCT p_id 
+                           FROM tree 
+                           WHERE p_id IS NOT NULL) THEN 'Leaf'
            ELSE 'Inner'
         END AS type
 FROM Tree
