0c3f6e5183698e4935a2685279eb8d704e69b176
Ashutosh Sharma <86849270+aashu1208@users.noreply.github.com>
2026-09-23 00:31:37 +0530

    Time: 1311 ms (86.06%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,23 @@
+(
+    SELECT u.name AS results
+    FROM Users u
+    JOIN MovieRating m
+        ON u.user_id = m.user_id
+    GROUP BY u.user_id, u.name
+    ORDER BY COUNT(*) DESC, u.name ASC
+    LIMIT 1
+)
+
+UNION ALL
+
+(
+    SELECT mv.title AS results
+    FROM Movies mv
+    JOIN MovieRating m
+        ON mv.movie_id = m.movie_id
+    WHERE m.created_at >= '2020-02-01'
+      AND m.created_at < '2020-03-01'
+    GROUP BY mv.movie_id, mv.title
+    ORDER BY AVG(m.rating) DESC, mv.title ASC
+    LIMIT 1
+);
